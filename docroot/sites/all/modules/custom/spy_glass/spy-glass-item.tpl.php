27c1f8ef19e4a7bc4e77aced37f164e4393c60d5
Drew Kimberly <andrew.e.kimberly@gmail.com>
2016-01-03 18:40:23 -0500

    Remove default docroot.

--- docroot/sites/all/modules/custom/spy_glass/spy-glass-item.tpl.php
+++
@@ -1,6 +0,0 @@
-<p>
-    <strong><?php print $title; ?></strong>
-    <br />
-    <?php print $data; ?>
-    <em><?php print $module_name; ?></em>
-</p>
--- docroot/sites/all/themes/custom/ninesixtyrobots/js/javascript.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    Drupal.behaviors.ninesixtyrobots = {
-        attach: function (context) {
-            $('#search-block-form input:text', context).autofill({
-                value: "Search..."
-            });
-        }
-    };
-})(jQuery);
