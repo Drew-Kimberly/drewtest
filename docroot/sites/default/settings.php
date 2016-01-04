6638c202c9d6ff0e4a2f3398acf632b0491e980b
Acquia Cloud <support@acquia.com>
2016-01-04 11:47:12 +0000

    Importing site archive

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -733,6 +733,11 @@ $databases['default']['default'] = array (
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = 'sites/drupal8.dd/files/config_wx5yP05cd2Z_uDWQqrBoL0mokwd3aDHRDHDRD-QGSkNR7RyOR7qctVixl09EnKGPR8-BIYuvGw/sync';
 
+
+// On Acquia Cloud, this include file configures Drupal to use the correct
+// database in each site environment (Dev, Stage, or Prod). To use this
+// settings.php for development on your local workstation, set $db_url
+// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
 if (file_exists('/var/www/site-php')) {
-    require '/var/www/site-php/drewtest/drewtest-settings.inc';
+  require('/var/www/site-php/drewtest/drewtest-settings.inc');
 }
