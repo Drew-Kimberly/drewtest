f6fcbeef47dccc3311e69e6fd80c445dbf760b4f
Drew Kimberly <andrew.e.kimberly@gmail.com>
2016-01-15 12:16:16 -0500

    Refactored RoarController to invoke RoarGenerator service from dependency injection container

--- docroot/sites/all/modules/custom/dino_roar/src/Controller/RoarController.php
+++ docroot/sites/all/modules/custom/dino_roar/src/Controller/RoarController.php
@@ -4,20 +4,36 @@ namespace Drupal\dino_roar\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class RoarController extends ControllerBase
 {
+    private $roarGenerator;
+
+    public function __construct(RoarGenerator $roarGenerator) {
+        $this->roarGenerator = $roarGenerator;
+    }
+
     public function roar($count) {
         $roarGen = new RoarGenerator();
 
         if ((int) $count > 0) {
-            $roar = $roarGen->createRoar($count);
+            $roar = $this->roarGenerator->createRoar($count);
         }
         else {
-            $roar = $roarGen->createRoar(3);
+            $roar = $this->roarGenerator->createRoar(3);
         }
 
         return new Response($roar);
     }
+
+    public static function create(ContainerInterface $container)
+    {
+        $roarGen = $container->get('dino_roar.roar_generator');
+
+        return new static($roarGen);
+    }
+
+
 }
